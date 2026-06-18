b4ff476b6dd7ec1bb8baed963b1ac96c1653040d
Jesus Lopez Aranda <jlopeza@gbm.com.mx>
2026-06-18 12:41:02 -0600

    add cards

--- config/api_router.py
+++ config/api_router.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from ehardware.cards.views import CardViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
@@ -7,6 +8,7 @@ from ehardware.users.api.views import UserViewSet
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("cards", CardViewSet)
 
 
 app_name = "api"
